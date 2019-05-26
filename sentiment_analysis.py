24fd3e530c0b6df67c6f110cb066ff0cc0ceaf9f
muhi-zatar <46324129+muhi-zatar@users.noreply.github.com>
2019-05-26 11:17:06 +0300

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -71,4 +71,15 @@ def naiive_bayes(training_features, labels_train, test_features, labels_test):
          % ( accuracy_score(labels_test, clf.predict(test_features))))
   
 if __name__== "__main__":
+  path ="imdb_master.xlxs"
+  reviews_train, labels_train, reviews_test, labels_test = get_data(path)
+  train = preprocess(reviews_train)
+  test = preprocess(reviews_test)
+  train_features = tf_idf(train, 1)
+  test_features = tf_idf(test, 0)
+  
+  logistic_regression(training_features, labels_train, test_features, labels_test)
+  svm(training_features, labels_train, test_features, labels_test)
+  naiive_bayes(training_features, labels_train, test_features, labels_test)
+  
   
